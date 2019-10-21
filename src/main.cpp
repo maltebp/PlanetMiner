bcbd8d86986af8912f0bef41427610afaa87ced8
maltebp <maltebp94@gmail.com>
2019-10-21 19:30:15 +0200

    implemented countdown

--- src/main.cpp
+++ src/main.cpp
@@ -18,17 +18,45 @@ ResourceStack resourceStack;
 
 vector<Gatherer*> gatherers;
 
+time_t remainingTime;
+
+void endGame(){
+    cout<<"Game over!"<<endl;
+    exit(0); 
+}
+
+
+void* startCountdownCb(void* time){
+    remainingTime = (time_t) time;
+
+    while(remainingTime > 0 ){
+        struct timespec time1, time2;
+        time1.tv_sec = 1;
+        nanosleep( &time1, &time2 );
+        remainingTime--;
+    }
+
+    endGame();
+}
+
+void startCountDown(time_t time){
+    pthread_t thread;
+    pthread_create(&thread, NULL, startCountdownCb, (void*) time);
+}
 
 
 string createStatusMsg(){
     ostringstream ss;
+    ss<<"Time remaining: "<<remainingTime<<" seconds"<<endl<<endl;
     ss<<"Resources: "<<resourceStack.getResources()<<endl;
     ss<<"Gatherers: "<<gatherers.size();
     return ss.str();
 }
 
+
 int main(){
 
+    startCountDown(60);
     string result = ""; 
 
     while(true){
@@ -38,11 +66,6 @@ int main(){
             if(args[0] == "exit") break;
             if(args[0] == "buy"){
                 gatherers.push_back( new Gatherer(TYPE_SIMPLE_BOT, resourceStack)  );
-            }
-            if(args[0] == "res"){
-                std::ostringstream ss;
-                ss<<"You have "<<resourceStack.getResources()<<" resources";
-                result = ss.str();
             }
         }
     }
