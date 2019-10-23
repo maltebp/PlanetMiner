f778fd4bdb8fd2ac84eb70b12e7445fb09375999
maltebp <maltebp94@gmail.com>
2019-10-23 10:15:17 +0200

    Added store table and new bots

--- include/mysleep.hpp
+++ include/mysleep.hpp
@@ -1,12 +1,16 @@
 #pragma once
 
 #include <time.h>
+#include <iostream>
 
 void fsleep(float time){
     float dec = time - (int)time;
     long decL = ((long) (dec*1000)) * 1000L; 
     struct timespec time1, time2;
     time1.tv_sec = (int) time;
-    time2.tv_nsec = decL; 
+    time2.tv_nsec = decL;
+    
+    std::cout<<"Ms: "<<decL/1000000.<<std::endl;
+
     nanosleep( &time1, &time2 );
 }
